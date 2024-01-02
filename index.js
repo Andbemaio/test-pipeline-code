8df816ef043226339c7ffa16549f744caeccd722
Andbemaio <107969444+Andbemaio@users.noreply.github.com>
2024-01-02 17:46:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
     const tenantId = process.env.TENANT_ID;
+    const tenantName = process.env.TENANT_NAME;
     response = {
         'statusCode': 200,
         'type': 'Create',
         'body': {
             'tenant_id': tenantId,
+            'tenant_name': tenantName,
         }
     }
     console.log(response)
